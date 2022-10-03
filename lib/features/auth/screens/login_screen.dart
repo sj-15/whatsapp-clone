3597d31d344c3c3196e2c71eabdd31f55838eaf6
Sourav Jana <15.sj.sourav@gmail.com>
2022-10-03 12:00:04 +0530

    login screen

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,5 +1,7 @@
+import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/common/utils/colors.dart';
+import 'package:whatsapp_clone/common/widget/custom_button.dart';
 
 class LoginScreen extends StatefulWidget {
   static const routeName = '/login-screen';
@@ -10,26 +12,70 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final phonecontroller = TextEditingController();
+  Country? country;
   @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    phonecontroller.dispose();
+  }
+
+  void pickCountry() {
+    showCountryPicker(
+        context: context,
+        onSelect: (Country _country) {
+          setState(() {
+            country = _country;
+          });
+        });
+  }
+
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Enter your phone number'),
         elevation: 0,
         backgroundColor: backgroundColor,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Text(
-            'Whatsapp will need to verfiy your phone number.',
-          ),
-          const SizedBox(height: 10),
-          TextButton(
-            onPressed: () {},
-            child: const Text('Pick Country'),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'Whatsapp will need to verfiy your phone number.',
+            ),
+            const SizedBox(height: 10),
+            TextButton(
+              onPressed: pickCountry,
+              child: const Text('Pick Country'),
+            ),
+            const SizedBox(height: 5),
+            Row(
+              children: [
+                if (country != null) Text('+${country!.phoneCode}'),
+                const SizedBox(width: 10),
+                SizedBox(
+                  width: size.width * 0.7,
+                  child: TextField(
+                    controller: phonecontroller,
+                    decoration: const InputDecoration(hintText: 'phone number'),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: size.height * 0.6),
+            SizedBox(
+              width: 90,
+              child: CustomButton(
+                text: 'NEXT',
+                onPressed: () {},
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Whatsapp UI',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor,
